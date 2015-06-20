0955543b0839a1d8e8912e736e10a1aa2b01aa82
GumpCode <gumpglh@qq.com>
2015-06-20 19:06:16 +0800

    chapter 1 1.3

--- QuotientImage.py
+++ QuotientImage.py
@@ -3,10 +3,10 @@ from numpy import *
 from scipy.ndimage import filters
 from pylab import *
 
-figure(1)
 im = array(Image.open('test.jpg').convert('L'))
 im1 = filters.gaussian_filter(im, 1.5)
 im1 = im/im1
+figure(1)
 imshow(im1)
 axis('equal')
 axis('off')
