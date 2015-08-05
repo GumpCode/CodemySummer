d39f0590bec111cd83ad919316058aaac902e989
GumpCode <gumpglh@qq.com>
2015-08-05 10:35:43 +0800

    homework_2

--- homework_2/calibrateCamera.py
+++ homework_2/calibrateCamera.py
@@ -5,11 +5,12 @@ import glob
 objp = np.zeros((5*9,3), np.float32)
 objp[:,:2] = np.mgrid[0:9, 0:5].T.reshape(-1,2)
 
+print objp
 objpoints = []
 imgpoints = []
 
 images = glob.glob('*.JPG')
-criteria = (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+criteria = (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 100, 0.00001)
 
 for fname in images:
     img = cv2.imread(fname)
